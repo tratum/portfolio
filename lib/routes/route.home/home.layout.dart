cb45d881a4838ccc06b774313e0e79420d9560b9
tratum <tratum.connect@gmail.com>
2025-09-11 01:58:48 +0530

    ui: made changes in the Desktop Layout UI to leave space between the navigation bar and the home section

--- lib/routes/route.home/home.layout.dart
+++ lib/routes/route.home/home.layout.dart
@@ -43,7 +43,7 @@ class _DesktopHomeLayoutState extends State<DesktopHomeLayout> {
               scrollDirection: Axis.vertical,
               controller: _sController,
               children: [
-                vLarge(context),
+                vMedium(context),
                 DesktopHomeSection(key: _homeSectionKey),
                 vXtraLarge(context),
                 DesktopTechStackSection(key: _techStackSectionKey),
